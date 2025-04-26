4961a9b30e460e486450b388aa57a2f9086417d9
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2025-04-26 14:37:01 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            abort(404);
+        });
+    });
+}
